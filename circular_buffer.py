68cf71bd2f690a17fdc3e71e7490051588543e53
walchko <walchko@users.noreply.github.com>
2017-07-19 04:34:59 +0000

    update

--- circular_buffer.py
+++ circular_buffer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+import sparkline
 
 class CircularBuffer(object):
 	def __init__(self, size):
@@ -7,9 +9,16 @@ class CircularBuffer(object):
 		self.index = 0
 		self.size = size
 		self._data = []
+		self.sum = 0.0
+		self.min = 1E900
+		self.max = -1E900
 
 	def push(self, value):
 		"""append an element"""
+		self.sum += value
+		self.max = value if value > self.max else self.max
+		self.min = value if value < self.min else self.min
+		
 		if len(self._data) == self.size:
 			self._data[self.index] = value
 		else:
@@ -26,14 +35,27 @@ class CircularBuffer(object):
 
 	def get_all(self):
 		"""return a list of all the elements"""
-		return(self._data)
+		# return(self._data)
+		ret = []
+		if self.index > 0:
+			ret = self._data[self.index:self.size] + self._data[0:self.index]
+		else:
+			ret = self._data
+		return ret
+
+	def get_last(self):
+		return self._data[self.index-1]
 
 
 if __name__ == "__main__":
-	cb = CircularBuffer(10)
+	cb = CircularBuffer(60)
 
-	for i in range(20):
+	for i in range(200):
 		cb.push(i)
 
 	print(cb.get_all())
 	print('get cb[7]', cb[7])
+	print('get cb[0]', cb[0])
+	print('get last', cb.get_last())
+	print('ine', cb.get_last(), sparkline.sparkify(cb.get_all()).encode('utf-8'))
+	print(cb.get_last(), sparkline.sparkify(cb.get_all()))
